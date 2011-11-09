4aa978d5f518844ccc85525c46890a8d106e07f3
jbfink <john.fink@gmail.com>
2011-11-09 15:59:53 -0500

    Working (commented out) code for quickadd in testpost.rb

--- testpost.rb
+++ testpost.rb
@@ -2,4 +2,12 @@ require 'google_calendar'
 cal = Google::Calendar.new(:username => ENV['GMAIL'],
 			  :password => ENV['GPASS'],
 			  :app_name => 'fillmore-hamiltontimes')
+# mostly messing with irb so you don't see much code BUT  the following
+#actually works! yay
+
+# event = cal.create_event do |e|
+# e.content = "Bar Mitzvah on January 30th"
+# e.quickadd = true
+#end
+
 
